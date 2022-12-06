33a51951afdb8edff36f090cc4d7d03e9e9a0409
Haechan Lee <dlgockssla@gmail.com>
2022-12-06 15:05:31 +0900

    logging request

--- src/controller.js
+++ src/controller.js
@@ -6,20 +6,20 @@ const { generateRandomString, parsePath, formatPath } = require("./util");
 const { STORAGE_BASE_URL, BUCKET_NAME, APP_NAME } = require("./const");
 
 const writeWeatherImageUrlToAin = async (req, res) => {
-    res.send("Triggered!");
+    console.log("req :>> ", req);
+    // console.log("req.body :>> ", req.body);
+    // console.log("req.body.transaction :>> ", req.body.transaction);
+    // const tx = req.body.transaction;
+    // validateTransaction(tx);
 
-    console.log("req.body :>> ", req.body);
-    console.log("req.body.transaction :>> ", req.body.transaction);
-    const tx = req.body.transaction;
-    validateTransaction(tx);
-
-    console.log("tx.tx_body_operation :>> ", tx.tx_body_operation);
-    const { ref, value: weather } = tx.tx_body.operation;
-    // app/appname/...
-    const parsedRef = parsePath(ref);
+    // console.log("tx.tx_body_operation :>> ", tx.tx_body_operation);
+    // const { ref, value: weather } = tx.tx_body.operation;
+    // // app/appname/...
+    // const parsedRef = parsePath(ref);
 
     // text-to-image
-    const prompt = `${weather} weather landscape with half hill and half sky , solid color, simple cartoon style`;
+    // const prompt = `${weather} weather landscape with half hill and half sky , solid color, simple cartoon style`;
+    const prompt = `Sunny weather landscape with half hill and half sky , solid color, simple cartoon style`;
     const { task_id: taskId } = await createTask(prompt);
     console.log("taskId :>> ", taskId);
 
@@ -35,7 +35,8 @@ const writeWeatherImageUrlToAin = async (req, res) => {
 
     // write image url to ain
     const storageImageUrl = `${STORAGE_BASE_URL}/${BUCKET_NAME}/${destFileName}`;
-    const backgroundPath = formatPath([...parsedRef.slice(2, parsedRef.length - 1), "background"]);
+    // const backgroundPath = formatPath([...parsedRef.slice(2, parsedRef.length - 1), "background"]);
+    const backgroundPath = "asdfew/background";
     await setValue(APP_NAME, backgroundPath, { value: storageImageUrl });
     console.log(`Ain: set url(${storageImageUrl}) at ${backgroundPath}`);
 };
